ddf5e5e5dcb90777180fd3522ec907dae5462129
Santosh Sridhar <santosh.sridhar1@wipro.com>
2017-05-05 20:59:34 +0530

    First Commit

---
+++ NewTest/src/test/java/com/wipro/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.wipro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		assertEquals(5,new Calculator().add(2, 3));
+	}
+
+}
